be9304ee0996f1f87ef35d5aaf5ef802cfb53d19
Rishabh Lakhotia <rishabh.l1@ahduni.edu.in>
2023-08-07 15:01:31 +0530

    LAB 1

---
+++ assignments/07.08.2023/2413.java
@@ -0,0 +1,13 @@
+lass Solution {
+    public int smallestEvenMultiple(int n) {
+        if (n%2==0){
+
+            return n;
+
+         }
+        else
+        {
+            return n*2;
+        }            
+    }
+}
